14c40e0f1c79b2dfe3cfe917befff5200758e790
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-27 23:11:01 -0700

    add useEffect and useState hooks to GameBoard

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const GameBoard = ({rows, cols}) => {
+
+    let {cellData, setCellData} = useState([]);
+
+    // initialize cellData to the specified number of rows and columns
+    useEffect(() => {
+
+        let gameBoard = [];
+        let row = [];
+
+        // initialize a row to all zeroes
+        for (let i = 0; i < cols; i++)
+            { row.push(0); }
+        
+        // add rows to gameboard
+        for (let i = 0; i < rows; i++)
+            { gameBoard.push(row); }
+
+        setCellData(gameBoard);        
+
+    }, []);
 
     return (
         <h1>GameBoard</h1>
