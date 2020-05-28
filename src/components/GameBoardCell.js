1f92cd31013b4d43b74980616a2bc1a3b8f75d4b
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-28 10:54:18 -0700

    style gameboard cells

--- src/components/GameBoardCell.js
+++ src/components/GameBoardCell.js
@@ -3,7 +3,7 @@ import React from "react";
 const GameBoardCell = ({value, row, col, toggleValue}) => {
 
     return (
-        <td onClick={() => toggleValue(row, col)}>{value}</td>
+        <td className={value ? "alive" : "dead"} onClick={() => toggleValue(row, col)}></td>
     )
 }
 
